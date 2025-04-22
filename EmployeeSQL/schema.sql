8c58cdca248603e088029017392655568694b538
Quentin Bartholomew <malcolmb98@yahoo.com>
2025-04-21 21:01:00 -0700

    Creat table statements, screenshots of successful imports, not null contraints

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,3 +1,41 @@
+CREATE TABLE Department (
+    dept_no VARCHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE Title (
+    title_id VARCHAR(5) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Employee (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(5) NOT NULL REFERENCES Title(title_id),
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL
+);
+
+CREATE TABLE Salary (
+    emp_no INT PRIMARY KEY REFERENCES Employee(emp_no),
+    salary INT NOT NULL
+);
+
+CREATE TABLE DeptEmp (
+    emp_no INT REFERENCES Employee(emp_no),
+    dept_no VARCHAR(4) REFERENCES Department(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE DeptManager (
+    dept_no VARCHAR(4) REFERENCES Department(dept_no),
+    emp_no INT REFERENCES Employee(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
+
+
 Department
 ----------
 dept_no PK varchar(4)
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# DA-Module-7-Challenge
+# DA-Module-9-Challenge
 
 ### Author
 Quentin Bartholomew
+
+#### Copy Commands
+```sql
+\copy Department FROM 'data/departments.csv' DELIMITER ',' CSV HEADER;
+\copy Title FROM 'data/titles.csv' DELIMITER ',' CSV HEADER;
+\copy Employee FROM 'data/employees.csv' DELIMITER ',' CSV HEADER;
+\copy Salary FROM 'data/salaries.csv' DELIMITER ',' CSV HEADER;
+\copy DeptEmp FROM 'data/dept_emp.csv' DELIMITER ',' CSV HEADER;
+\copy DeptManager FROM 'data/dept_manager.csv' DELIMITER ',' CSV HEADER;
